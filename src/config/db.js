c0dfdadc6d4c5f9a0720c8d793ce2f93e54f0b50
Staichak4 <pedro.staichaka.19@gmail.com>
2024-10-16 17:41:05 -0300

    prova

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async() => {
     try {
-        await mongoose.connect(`mongodb://localhost:27017`);
+        await mongoose.connect(process.env.URI);
         console.log('MongoDB conectado com sucesso.')
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB: ', error);
